8fb5664d6a87a7eee1c0ef1711031d36b0a8beb9
shaye <sm0548540902@gmail.com>
2025-12-10 17:04:21 +0200

    add function show by name

--- services/utils.js
+++ services/utils.js
@@ -1,7 +1,20 @@
 import { tasks } from "../data/tasks.js";
+import readLineSync from "readline-sync"
 
 export function addTask(task){
 if (typeof task === 'object'){
     tasks.push(task)
 }
+}
+
+export function showByName(){
+    const name = readLineSync.question("which name: ")
+    for (let p of tasks){
+        if (p.name.toLowerCase() === name.toLowerCase()){
+            for (let key in p){
+                console.log(key,p[key]);
+                return
+            }
+        }
+    }
 }
